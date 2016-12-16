5550488a27158a1bf83c97fff835aa30c7b7f03e
Tomasz Belina <tomasz.belina@qualent.eu>
2016-12-16 19:13:08 +0100

    dodaj wyszukiwanie

--- js/sortowanie.js
+++ js/sortowanie.js
@@ -4,8 +4,62 @@
     
     var table = document.querySelector("#myTable"),
         ths = table.querySelectorAll("thead th"),
-        trs = table.querySelectorAll("tbody tr");
+        trs = table.querySelectorAll("tbody tr"),
+        szukaj = document.querySelector("input.szukaj");
     
+    
+    
+    szukaj.addEventListener("keydown", function(e){
+    
+        var name = szukaj.value + e.key;
+        
+        var regex = RegExp(name + "+", "ig");
+      
+        
+        var trsArr = makeArray(trs),
+            df = document.createDocumentFragment();
+        
+        console.log(name);
+        trsArr.forEach(function(tr){
+            
+            tr.classList.remove("hidden");
+            var td = tr.children[1].textContent;
+            if(name.slice(1) !== "Backspace" && /*regex.test(td)===false*/td !== name){
+                df.appendChild(tr);
+                tr.classList.add("hidden");
+            }
+            df.appendChild(tr);
+        });
+        table.querySelector("tbody").appendChild(df);
+    });
+                            
+    szukaj.addEventListener("keypress", function(e){
+        
+        var name = szukaj.value + e.key;
+        
+        var regex = RegExp(name + "+", "ig");
+        
+        var trsArr = makeArray(trs),
+            df = document.createDocumentFragment();
+        
+        
+        
+        trsArr.forEach(function(tr){
+          
+            tr.classList.remove("hidden");
+            var td = tr.children[1].textContent;
+            
+            console.log(regex.test(td));
+            
+            if(/*regex.test(td)==='false'*/td !== name){
+                df.appendChild(tr);
+                tr.classList.add("hidden");
+            }
+            df.appendChild(tr);
+        });
+        table.querySelector("tbody").appendChild(df);
+        
+    });
 
     function makeArray(nodeList){
         
@@ -59,8 +113,7 @@
         target.className = order;
         
         table.querySelector("tbody").appendChild(df);
-        
-        console.log(trsArr);
+       
     }
     
     for (var i = 0; i < ths.length; i++) {
