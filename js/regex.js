ccf68cc9a7a81751965d9f09b9fbb964c067fbb0
Tomasz Belina <tomasz.belina@qualent.eu>
2016-12-26 09:21:42 +0100

    regex

--- js/regex.js
+++ js/regex.js
@@ -8,7 +8,7 @@
       this._regex = new RegExp("(" + this._words.join("|") +")", "igm");
       
       this._assignEvents();
-  };
+  }; 
     
   WordKiller.prototype._assignEvents = function(){
      
@@ -17,9 +17,26 @@
     
   WordKiller.prototype._filterMessage = function(e){
    
-      console.log(e);
+      this._field.value = this._field.value.replace(this._regex, function(match){
+          return this._cenzoreWords(match);
+      }.bind(this));
+     
   };
+    
+  WordKiller.prototype._cenzorSigns = "!@#$^*".split("");
+    
+  WordKiller.prototype._cenzoreWords = function(word){
+
+      var cenzored = "",
+          random = 0;;
       
+      for(var i=0; i < word.length; i++){
+          random = Math.round(Math.random()*(this._cenzorSigns.length - 1 - 0) + 0)
+          cenzored += this._cenzorSigns[random]; 
+      }
+      
+      return cenzored;
+  };
     
       
   var wk = new WordKiller(document.querySelector("[name='regexMessage']"), 
